ddd45f65f9ea3cea86523b4a166696c787d7821d
invisible0831 <amirsoli80@gmail.com>
2021-10-16 19:44:35 +0330

    dfa module complete

--- dfa.py
+++ dfa.py
@@ -1,33 +1,35 @@
-from typing import Tuple, List
+from typing import Dict, Tuple, List, Union
+import re
 
 
 class DFA:
 
-    def __init__(self, transitions: List[Tuple[int, int, str]], final_states: set):
+    def __init__(self, transitions: Dict[int, List[Tuple[int, str]]], final_states: Dict[int, Tuple[str, bool]]):
         '''
 
-        :param transitions: first_state , final_state, regex for transition between states
+        :param transitions: state_number: list[next_state, regex for transition between states]
+        param final_states: final_state_number: token_name, retract_pointer boolean
         '''
-        self.transitions = transitions
+        self.transition_dict = transitions
         self.final_states = final_states
         self.current_state = 0
 
-    def move(self, current_char: str) -> int:
-        for transition in self.transitions:
-            pass
-        return 0
+    def move(self, current_char: str) -> None:
+        for next_state, regex in self.transition_dict.get(self.current_state, []):
+            if re.match(regex, current_char):
+                self.current_state = next_state
+                if self.current_state in self.final_states:
+                    raise FinalState(*self.final_states[self.current_state])
+        else:
+            raise CannotMove()
 
 
 class FinalState(Exception):
 
-    def __init__(self, state_number: int, ):
-        pass
+    def __init__(self, token_name: str, retract_pointer: bool):
+        self.token_name = token_name
+        self.retract = retract_pointer
 
-
-class MiddleState(Exception):
-    pass
 
 class CannotMove(Exception):
     pass
-
-
