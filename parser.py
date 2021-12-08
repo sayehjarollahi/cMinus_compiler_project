1dd0bbbbcc74316f03d041f4ce3c2a0c332d515c
invisible0831 <amirsoli80@gmail.com>
2021-12-08 19:36:19 +0330

    parser started

---
+++ parser.py
@@ -0,0 +1,14 @@
+class Parser:
+    # stack, transition diagram, func next_state,
+    def __init__():
+        pass
+
+    '''def next_state:
+        for state, edge in neighbor_states[self.state]:
+            if token in first(edge):
+                return edge, state
+            elif epsilon in first(edge):
+                if next_state(state):
+                    return edge, state  #queue
+        return False'''
+    # def handle_error()
---
+++ transition_diagram.py
@@ -0,0 +1,14 @@
+class NonTerminal:
+    pass
+    '''staring node of diagram, first, follow'''
+
+
+class Node:
+    pass
+    '''func is_final
+        number
+        list[tuple(edge, next_node)]
+        '''
+
+
+EPSILON = 'epsilon'
